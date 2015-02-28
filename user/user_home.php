c6fea45e942866d9b8505aec07e355f15e3d0c45
AmiraAli <amira_ali_marzouk@outlook.com>
2015-02-28 17:13:47 +0200

    app configuration

--- user/user_home.php
+++ user/user_home.php
@@ -1,8 +1,14 @@
 <?php
 
+require 'user_model.php';
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+//testing user_orm
+$obj = user_ORM::getInstance();
+$obj->setTable('student');
+echo $obj->insert(array('id'=>4,'name' => 'amira', 'age' => 20,'track'=> 'os'));
 
--- user/user_model.php
+++ user/user_model.php
@@ -1,8 +1,58 @@
 <?php
 
+require 'appconf.php';
+
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+class user_ORM {
 
+     static $conn;
+    private $dbconn;
+    protected $table;
+    
+    static function getInstance(){
+        if(self::$conn == null){
+            self::$conn = new user_ORM();
+        }
+        return self::$conn;
+    }
+    
+    function __construct() {
+        
+        extract($GLOBALS['conf']);
+        $this->dbconn = new mysqli($host, $username, $password, $database);
+        
+    }
+    
+    function getConnection(){
+        return $this->dbconn;
+    }
+    
+    function setTable($table){
+        $this->table = $table;
+    }
+
+     function insert($data){
+        $query = "insert into $this->table set ";
+        foreach ($data as $col => $value) {
+            $query .= $col."= '".$value."', ";
+            
+        }
+        $query[strlen($query)-2]=" ";
+        $state = $this->dbconn->query($query);
+        if(! $state){
+            return $this->dbconn->error;
+        }
+        
+        return $this->dbconn->affected_rows;
+        
+    }
+    function __destruct() {
+        mysqli_close($this->dbconn);
+        
+    }
+
+}
