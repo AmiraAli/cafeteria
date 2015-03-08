889c1f1a13a7a5fe78af0b17e5ee6b8d19fe06df
AmiraAli <amira_ali_marzouk@outlook.com>
2015-03-08 19:53:53 +0200

    add catogries

--- admin/add_product.php
+++ admin/add_product.php
@@ -4,123 +4,119 @@ require '../model/model.php';
 require './admin_header.php';
 
 
-                $valid = new validator();
-                if (!empty($_GET)) {
-                    $product_data = explode(",", $_GET['product1']);
+$valid = new validator();
+if (!empty($_GET)) {
+    $product_data = explode(",", $_GET['product1']);
+}
+
+if (!empty($_POST['save'])) {
+    if (empty($_POST['product_update'])) {
+
+        $flag = true;
+
+        // check on validation 
+        $check = $valid->empty_fields($_POST);
+        if (gettype($check) == "array") {
+            for ($i = 0; $i < count($check); $i++) {
+                $check[$i] . "<br/>";
+            }
+            $flag = false;
+        }
+
+        if (count($check) == 1 && $check[0] == 'product_update is required') {
+            $flag = true;
+        }
+
+        //check on image 
+        $error_image = $valid->valid_image($_FILES['productfile']['error'], $_FILES['productfile']['type']);
+        if (gettype($error_image) == "string") {
+            $flag = false;
+        }
+
+        if ($flag == true) {
+
+            //save image 
+
+            $upfile = '/var/www/cafeteria/images/products/' . $_FILES['productfile']['name'];
+            if (is_uploaded_file($_FILES['productfile']['tmp_name'])) {
+                if (!move_uploaded_file($_FILES['productfile']['tmp_name'], $upfile)) {
+                    echo 'can`t upload your image  ' . "<br/>";
                 }
-                
-                if (!empty($_POST['save'])) {
-                     if (empty($_POST['product_update'])) {
-                         
-                    $flag = true;
-
-                    // check on validation 
-                    $check = $valid->empty_fields($_POST);
-                    if (gettype($check) == "array") {
-                        for ($i = 0; $i < count($check); $i++) {
-                             $check[$i] . "<br/>";
-                        }
-                        $flag = false;
-                    }
-                    
-                    if (count($check) == 1 && $check[0] == 'product_update is required') {
-                    $flag = true;
-                }
-
-                    //check on image 
-                    $error_image = $valid->valid_image($_FILES['productfile']['error'], $_FILES['productfile']['type']);
-                    if (gettype($error_image) == "string") {
-                        $flag = false;
-                    }
-
-                    if ($flag == true) {
-
-                        //save image 
-
-                        $upfile = '/var/www/cafeteria/images/products/' . $_FILES['productfile']['name'];
-                        if (is_uploaded_file($_FILES['productfile']['tmp_name'])) {
-                            if (!move_uploaded_file($_FILES['productfile']['tmp_name'], $upfile)) {
-                                echo 'can`t upload your image  ' . "<br/>";
-                            }
-                        }
+            }
 
 
-                        // insert data into database 
-                        $obj_category = ORM::getInstance();
-                        $obj_category->setTable('categories');
-                        $all_data=$obj_category->select(array('name' => $_POST['category']));
-                        $current_cat = $all_data->fetch_assoc();
-                        $id=$current_cat['id'];
-                        
-                        $obj = ORM::getInstance();
-                        $obj->setTable('products');
-                        //var_dump($_POST);
-                       // exit;
-                        if(empty($_POST['checkbox'])){
-                            $is_avaliable="0";
-                        }else{
-                            $is_avaliable=$_POST['checkbox'];
-                        }
-                         $obj->insert(array("name" => $_POST['product'], "price" => $_POST['price'], "category_id"=>$id, "is_available"=>$is_avaliable,"pic" => $_FILES['productfile']['name']));
-                        header("Location: http://localhost/cafeteria/admin/all_products.php");
+            // insert data into database 
+            $obj_category = ORM::getInstance();
+            $obj_category->setTable('categories');
+            $all_data = $obj_category->select(array('name' => $_POST['category']));
+            $current_cat = $all_data->fetch_assoc();
+            $id = $current_cat['id'];
+
+            $obj = ORM::getInstance();
+            $obj->setTable('products');
+            //var_dump($_POST);
+            // exit;
+            if (empty($_POST['checkbox'])) {
+                $is_avaliable = "0";
+            } else {
+                $is_avaliable = $_POST['checkbox'];
+            }
+            $obj->insert(array("name" => $_POST['product'], "price" => $_POST['price'], "category_id" => $id, "is_available" => $is_avaliable, "pic" => $_FILES['productfile']['name']));
+            header("Location: http://localhost/cafeteria/admin/all_products.php");
 //                        mysqli_close($db);
-                    }
-                }
-               else{
-                $flag = true;
-                $check = $valid->empty_fields($_POST);
-                if (gettype($check) == "array") {
-                    $flag = false;
-                    for ($i = 0; $i < count($check); $i++) {
-                        $check[$i];
-                    }
-                }
-
-                $error_image = $valid->valid_image($_FILES['productfile']['error'], $_FILES['productfile']['type']);
-                if (gettype($error_image) == "string") {
-                    $flag = false;
-                }
-
-                //check on password&email&image 
-               
-                if ($flag == true) {
-                    $upfile = '/var/www/cafeteria/images/products/' . $_FILES['productfile']['name'];
-                    if (is_uploaded_file($_FILES['productfile']['tmp_name'])) {
-                        if (!move_uploaded_file($_FILES['productfile']['tmp_name'], $upfile)) {
-                            echo 'can`t upload your image  ' . "<br/>";
-                            exit();
-                        }
-                    }
-
-                $obj_category = ORM::getInstance();
-                $obj_category->setTable('categories');
-                $all_data=$obj_category->select(array('name' => $_POST['category']));
-                $current_cat = $all_data->fetch_assoc();
-                $id=$current_cat['id'];
-                        
-                        
-                $product_data = explode(",", $_POST['product_update']);
-                $obj = ORM::getInstance();
-                $obj->setTable('products');
-                if(empty($_POST['checkbox'])){
-                            $is_avaliable="0";
-                        }else{
-                            $is_avaliable=$_POST['checkbox'];
-                        }
-                
-                $updated = $obj->update(array('id' => $product_data[0]), array("name" => $_POST['product'], "price" => $_POST['price'], "category_id"=>$id, "is_available"=>$is_avaliable,"pic" => $_FILES['productfile']['name']));
-                echo $updated;
-                header("Location: http://localhost/cafeteria/admin/all_products.php");
+        }
+    } else {
+        $flag = true;
+        $check = $valid->empty_fields($_POST);
+        if (gettype($check) == "array") {
+            $flag = false;
+            for ($i = 0; $i < count($check); $i++) {
+                $check[$i];
             }
-            else{
-                $product=$_POST['product_update'];
-                header("Location: http://localhost/cafeteria/admin/add_product.php?product1=$product");
+        }
+
+        $error_image = $valid->valid_image($_FILES['productfile']['error'], $_FILES['productfile']['type']);
+        if (gettype($error_image) == "string") {
+            $flag = false;
+        }
+
+        //check on password&email&image 
+
+        if ($flag == true) {
+            $upfile = '/var/www/cafeteria/images/products/' . $_FILES['productfile']['name'];
+            if (is_uploaded_file($_FILES['productfile']['tmp_name'])) {
+                if (!move_uploaded_file($_FILES['productfile']['tmp_name'], $upfile)) {
+                    echo 'can`t upload your image  ' . "<br/>";
+                    exit();
+                }
             }
-               }
-                
+
+            $obj_category = ORM::getInstance();
+            $obj_category->setTable('categories');
+            $all_data = $obj_category->select(array('name' => $_POST['category']));
+            $current_cat = $all_data->fetch_assoc();
+            $id = $current_cat['id'];
+
+
+            $product_data = explode(",", $_POST['product_update']);
+            $obj = ORM::getInstance();
+            $obj->setTable('products');
+            if (empty($_POST['checkbox'])) {
+                $is_avaliable = "0";
+            } else {
+                $is_avaliable = $_POST['checkbox'];
+            }
+
+            $updated = $obj->update(array('id' => $product_data[0]), array("name" => $_POST['product'], "price" => $_POST['price'], "category_id" => $id, "is_available" => $is_avaliable, "pic" => $_FILES['productfile']['name']));
+            echo $updated;
+            header("Location: http://localhost/cafeteria/admin/all_products.php");
+        } else {
+            $product = $_POST['product_update'];
+            header("Location: http://localhost/cafeteria/admin/add_product.php?product1=$product");
+        }
     }
-                
-                ?>
+}
+?>
 
 
 
@@ -129,10 +125,10 @@ require './admin_header.php';
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        
+
         <link rel="stylesheet" href= "../bootstrap-3.3.2-dist/css/bootstrap.css">
         <link rel="stylesheet" href= "../bootstrap-3.3.2-dist/css/bootstrap-theme.css">
-        
+
         <script src="../jquery-2.1.3.min.js" type="text/javascript"></script>
         <script src="../bootstrap-3.3.2-dist/js/bootstrap.js" type="text/javascript"></script>
 
@@ -144,22 +140,24 @@ require './admin_header.php';
 
         <div class="container">
             <div class="row"> 
-               <div class="col-md-offset-4 col-md-4">
+                <div class="col-md-offset-4 col-md-4">
 
 
                     <form  method='post' action='add_product.php' enctype="multipart/form-data" class="form-signin">
-                    <input type="hidden" name="product_update" value="<?php if (isset($_GET['product1'])) {
-                        echo $_GET['product1'];
-                    } ?>">
+                        <input type="hidden" name="product_update" value="<?php
+                        if (isset($_GET['product1'])) {
+                            echo $_GET['product1'];
+                        }
+                        ?>">
                         <h4> Add Product </h4>
                         <div class="form-group">
                             <label>Product</label>
-                            <input class="form-control" type='text' name='product'placeholder="Enter product name..." value="<?php 
-                             if (!empty($_POST['product'])) {
-                                    echo $_POST['product'];
-                             }
-                             
-                             if (!empty($_GET)) {
+                            <input class="form-control" type='text' name='product'placeholder="Enter product name..." value="<?php
+                            if (!empty($_POST['product'])) {
+                                echo $_POST['product'];
+                            }
+
+                            if (!empty($_GET)) {
                                 echo $product_data[1];
                             }
                             ?>" >
@@ -173,15 +171,15 @@ require './admin_header.php';
 
                         <div class="form-group">
                             <label>Price</label> 
-                            <input class="form-control" type='number' name='price'placeholder="Enter price here..." value="<?php 
-                             if (!empty($_POST['price'])) {
-                                    echo $_POST['price'];
-                                }
-                                 if (!empty($_GET)) {
+                            <input class="form-control" type='number' name='price'placeholder="Enter price here..." value="<?php
+                            if (!empty($_POST['price'])) {
+                                echo $_POST['price'];
+                            }
+                            if (!empty($_GET)) {
                                 echo $product_data[2];
                             }
                             ?>">  
-                             <span> <?php
+                            <span> <?php
                                 if (isset($check[1]) && (empty($_POST['price']))) {
                                     echo " This field is required ";
                                 }
@@ -197,25 +195,25 @@ require './admin_header.php';
                                 /**
                                  * get all categories from datbase 
                                  */
-                                $obj_categories =ORM::getInstance();
+                                $obj_categories = ORM::getInstance();
                                 $obj_categories->setTable('categories');
 
                                 $all_categories = $obj_categories->select_all();
-                                
-                       
+
+
                                 if ($all_categories->num_rows > 0) {
 
-                                    for ($i=0;$i<$all_categories->num_rows;$i++){
-                                            $category = $all_categories->fetch_assoc();
-                                      
+                                    for ($i = 0; $i < $all_categories->num_rows; $i++) {
+                                        $category = $all_categories->fetch_assoc();
+
                                         //foreach ($category as $key => $value) {
-                                            ?> 
-                                            <option name="option" value="<?php echo trim($category['name']) ?>"> <?php echo $category['name'] ?>
+                                        ?> 
+                                        <option name="option" value="<?php echo trim($category['name']) ?>"> <?php echo $category['name'] ?>
                                             <?php
-                                       // }
-                                    }
-                                } else {
-                                    ?>
+                                            // }
+                                        }
+                                    } else {
+                                        ?>
                                     <option> no category </option>  
                                     <?php
                                 }
@@ -224,14 +222,8 @@ require './admin_header.php';
                             </select>
                             <button  class="form-control"class="btn btn-default btn-xs"   value="text"type="button" onclick="addText();">add category</button>
                             <div id="div2"></div> 
-                            <script type="text/javascript">
-                                function addText()
-                                        {
-                                        var div1=document.getElementById("div2");
-                                        div1.innerHTML="<input type='text'/>";
-                                        }
-                                 </script>
-                            
+
+
                         </div>
 
                         <div class="form-group">
@@ -250,7 +242,7 @@ require './admin_header.php';
                             <label>  
                                 <input type="checkbox" name="checkbox" value="1"> Is available
                             </label>
-                            
+
                         </div>
 
                         <input class="btn btn-success btn-sm" type='submit' name='save' value='Save'>
@@ -261,10 +253,52 @@ require './admin_header.php';
                         </div>
                         </div>
                         </div>
-       
 
-</form>
-</body>
-</html>
+
+                    </form>
+                    <script type="text/javascript">
+                        function addText()
+                        {
+                            var div1 = document.getElementById("div2");
+
+                            var elem_input_text = document.createElement("input");
+                            elem_input_text.setAttribute("type", "text");
+                            elem_input_text.setAttribute("id", "new_catogrie");
+
+                            var elem_btn = document.createElement("button");
+                            elem_btn.setAttribute("onclick", "add_catogrie()");
+                            elem_btn.setAttribute("class", "btn  btn-warning");
+
+                            elem_btn.innerHTML = "ADD";
+
+                            div1.appendChild(elem_input_text);
+                            div1.appendChild(elem_btn);
+
+                        }
+
+
+                        function add_catogrie() {
+                            var text = document.getElementById("new_catogrie").value;
+                            
+                            //open xmlhttp request that render to add_catogrie and send text
+                            var xmlhttp = new XMLHttpRequest();
+                            xmlhttp.open("POST", "add_catogrie.php", true);
+                            xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+                            xmlhttp.send("text="+ text);
+
+                            //on change check even the request send or not and get the values of response
+
+                            xmlhttp.onreadystatechange = function () {
+
+                                if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+
+                                    //get information of response of orders
+                                   // alert(xmlhttp.responseText);
+                                }
+                        };
+                    }
+                    </script>
+                    </body>
+                    </html>
 
 
